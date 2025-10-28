b3d03555e6751cb2502fbd0a4e4715e72733b668
ivanmaxfx <ivanmaxfx@gmail.com>
2025-10-28 15:47:19 +0200

    week1: fix MEM line (force C locale)

--- week1/node_health.sh
+++ week1/node_health.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 echo "TIME: $(date -Is)"
 echo "UPTIME: $(uptime -p)"
 echo "LOAD: $(cut -d' ' -f1-3 /proc/loadavg)"
-echo "MEM: $(free -m | awk '/Mem:/ {printf "used=%sMB total=%sMB\n", $3, $2}')"
+MEM_LINE=$(LC_ALL=C free -m | awk 'NR==2 {printf "used=%sMB total=%sMB", $3, $2}')
+echo "MEM: ${MEM_LINE}"
 echo "DISK(/): $(df -h / | awk 'NR==2 {print $3"/"$2" used ("$5")"}')"
